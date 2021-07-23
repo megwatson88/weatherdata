7f2faebcd66d33aed26940f473ef57fc3a23a8f5
megan.campbell.1988@gmail.com <megan.campbell.1988@gmail.com>
2021-07-22 22:40:00 -0700

    js additions and HTML

--- assests/javascript.js
+++ assests/javascript.js
@@ -1,2 +1,32 @@
-fetch 
-fetch 
+// let cityName = document.getElementById("city-name")
+// function myFunction(){
+//   fetch(`api.openweathermap.org/data/2.5/weather?q=lompoc&appid=0653198af3dba7668f4d7e512b654c07`)  
+//   .then(function(weatherResponse){
+//       return weatherResponse.json();
+//       console.log(weatherResponse);}
+//   }).then(funtion(weatherData) {
+//       console.log(weatherData);
+//   });
+//   fetch()
+//   .then Response => resizeBy.json()
+//   then(data)
+// }
+
+//fetch(``)
+
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Bakersfield&appid=0653198af3dba7668f4d7e512b654c07')
+  .then(function(res) {
+   return res.json();})
+  .then(function(data) {
+     console.log(data); })
+     //do this to reatreeve data ^
+    //let lat = data.coord.lat;
+    //let lon = data.coord.lon;
+   // let temp = data.main.temp;
+   // document.getElementById('temp').textContent = temp;
+   // fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=<API_KEY>`)
+    //  .then(res => res.json())
+     // .then(data => {
+      //  console.log(data); //do this to get data for the forloop for the 5 days 
+ //   });
+//})
