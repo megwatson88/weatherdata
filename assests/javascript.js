d0ad38e2f85fcf1b2cb24205fa47efd647c656c8
megan.campbell.1988@gmail.com <megan.campbell.1988@gmail.com>
2021-07-25 21:04:55 -0700

    additions

--- assests/javascript.js
+++ assests/javascript.js
@@ -3,7 +3,7 @@ function getCityName() {
     let cityName = document.getElementById("city-name").value;
     //cityName.addEventListener("click", value);
     console.log(cityName);
-    //begin the nested fetch
+    //begin the nested fetch to get data for all the weather 
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=0653198af3dba7668f4d7e512b654c07&units=imperial`)
         .then(function (weatherResponse) {
             return weatherResponse.json();
@@ -20,24 +20,34 @@ function getCityName() {
             document.getElementById('wind-speed').textContent = `${windSpeed} mph`;
             document.getElementById('humidity').textContent = `${humidity} % humidity`;
             //need UV index
-
+            //pulling the data for 5 day
             fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=0653198af3dba7668f4d7e512b654c07&units=imperial`)
                 .then(function (weatherResponseFiveDay) {
                     return weatherResponseFiveDay.json();
                 }).then(function (weatherDataFiveDay) {
                     console.log(weatherDataFiveDay)
-
+                    //color changes for UV index 
                     let uvi = weatherDataFiveDay.current.uvi;
                     document.getElementById("UV-Index").textContent = `${uvi} UV Index`;
-                    //let tomrrow = weatherResponseFiveDay.daily.1.temp.day;
-                    //console.log(tomrrow)
-        //             $("#today").html(`<div>
-        // <h1>City: ${response.name}<span><img src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png"</span></h1>
-        // <h6>Temp: ${response.main.temp}F°</h6>
-        // <h6>Wind Speed: ${response.wind.speed}mph</h6>
-        // <h6>Weather: ${response.weather[0].description}</h6>
-        // <h6>Humidity: ${response.main.humidity}%</h6>
-        // </div>`)
+                    // if (uvi < 3) {
+                    //     uvi.attr("id", "UV-Index");
+                    // } else if (uvi < 6) {
+                    //     uvi.attr("id", "UV-Index");
+                    // } else if (uvi > 10) {
+                    //     uvi.attr("id", "UV-Index");
+                    // };
+                    //next 5 day forcast begins here 
+                    let futureWeather = []
+
+                    //already searched citys 
+                    let searchedCities = JSON.parse(localStorage.getItem("Previous-Cities"))
+                    function pastSearchedCitied(){
+                        if (searchedCities[0] !== undefined){
+                            city = searchedCities[0];
+                            document.getElementById('temp').textContent = `${temp} degree F`;
+                        }
+                    }
+
                 });
             //  console.log(data); the temperature, the wind speed, and the humidity
             //do this to get data for the forloop for the 5 days   
